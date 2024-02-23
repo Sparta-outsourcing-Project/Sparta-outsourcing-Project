2a4b3d02095285b1aca9c8df85d3b0c2b113df5f
illu_y <ii_2002@naver.com>
2024-02-23 23:51:13 +0900

    ️setting : main setting

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function Main() {
-  return <div>MIAN</div>;
+  return (
+    <MainWrap>
+      <section></section>
+    </MainWrap>
+  );
 }
+
+export const MainWrap = styled.main`
+  width: 100%;
+`;
