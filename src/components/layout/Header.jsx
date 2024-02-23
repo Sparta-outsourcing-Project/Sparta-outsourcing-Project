7faa06d41ce7eeb2be5d4920cab307af633feaf8
illu_y <ii_2002@naver.com>
2024-02-23 23:45:45 +0900

    feat : a태그 Link로 변경

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { BrowserRouter as Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
@@ -26,7 +26,7 @@ export const HeaderWrap = styled.header`
 `;
 export const Logo = styled.h1`
   width: 200px;
-  & > img {
+  & > a > img {
     width: 100%;
   }
 `;
