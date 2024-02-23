6ecff6b3441457438ca550f6c0b6d89b1398eb7e
illu_y <ii_2002@naver.com>
2024-02-23 22:46:35 +0900

    [setting] header setting

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function Header() {
-  return <div>HEADER</div>;
+  return (
+    <header>
+      <a href="#">LOGO</a>
+    </header>
+  );
 }
+
+export const Header = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
