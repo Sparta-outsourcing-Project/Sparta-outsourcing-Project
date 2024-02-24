e5f40818b86522af3731c8fcd108323f5b6bfeaa
Hyeryeon-Seo <0168968060@naver.com>
2024-02-25 01:26:27 +0900

    feat: 기능구현중 in slider data api

--- src/api/mainSliderDataApi.js
+++ src/api/mainSliderDataApi.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 // 메인 유튜버 영상 슬라이더에 사용될 youtube data api
 
 export const mainSliderDataClient = axios.create({
-  baseURL: `https://youtube.googleapis.com/youtube/v3/videos`,
+  baseURL: `https://youtube.googleapis.com/youtube/v3`,
+  // https://youtube.googleapis.com/youtube/v3/channels?
   // videos 까지 쓰고  아래에다 ?부터써줘야만 part=snippet 해당되는 데이터가 뜸
   headers: {
     'Content-Type': 'application/json'
@@ -10,18 +11,40 @@ export const mainSliderDataClient = axios.create({
 });
 
 // 영상 썸네일 이미지
-export const getThumbnail = async (id) => {
+export const getThumbnail = async (videoId) => {
+  // 영상id받아서 채널id까지 받을 수 있으니 한번에 채널id로 연결시켜서
+  // 채널id통해 유튜버(채널)명, 구독자수 등 객체로 전달해주기
   // getThumbnail('영상id')로 사용하기
-  const { data } = await mainSliderDataClient.get(`?key=${import.meta.env.VITE_APP_API_KEY}&part=snippet&id=${id}`);
-  const thumbnailUrl = data.items[0].snippet.thumbnails.standard.url;
-  const a = a;
-  erer;
-  f;
+  const { data } = await mainSliderDataClient.get(
+    `/videos?part=snippet&id=${videoId}&key=${import.meta.env.VITE_APP_API_KEY}`
+  );
+  const snippet = data.items[0].snippet;
+  const channelId = snippet.channelId;
+  const channelTitle = snippet.channelTitle;
+  const thumbnailUrl = snippet.thumbnails.standard.url;
+
+  console.log(channelId);
+  console.log(channelTitle);
+  console.log(snippet);
+
   return thumbnailUrl;
+  {
+  }
 };
 
-// get 유튜버 이름
+// r_E_-FRiyKc
+//RLURZtX-fUo
+// get 유튜버명(채널명) - 위에서 같이 해도 될듯? (굳이 여러번 id안넣어도) // 아님 따로 쓰는게 편할까?
+export const getChannelTitle = async () => {};
 
 // get 구독자 수
+// 채널id UCE6e9tjKl3l3nGlAcxH7GQg
+// 근데 채널ID를 위에서 반환해주고 다시 넣기보다는.. 아예 같은 함수내에서 처리하는게 편할거 같은데.
+export const getChannels = async (channelId) => {
+  const { data } = await mainSliderDataClient.get(
+    `/channels?part=snippet&id=${channelId}&key=${import.meta.env.VITE_APP_API_KEY}`
+  );
+  console.log(data);
+};
 
-// get 평균 시청수 (기간?)
+// get 평균 시청수 (기간? 기간정해서 )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,17 @@ import Header from '../components/layout/Header';
 import Footer from '../components/layout/Footer';
 import Main from '../components/main/Main';
 import styled from 'styled-components';
+import { getChannels, getThumbnail } from '../api/mainSliderDataApi';
 
 export default function Home() {
+  const testSliderApi = async () => {
+    const url = await getThumbnail('RLURZtX-fUo');
+    const channel = await getChannels('UCE6e9tjKl3l3nGlAcxH7GQg');
+    return [url, channel];
+  };
+  const arr = testSliderApi();
+  console.log(arr);
+
   return (
     <Wrap>
       <Header />
