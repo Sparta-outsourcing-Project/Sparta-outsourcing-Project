86e3b704e30001c1d497d3a669636b69ac60af92
miju <im.ymiju@gmail.com>
2024-02-25 01:13:51 +0900

    Add: make hook MostPopularChannel, make api, make request, make Thumbnail Component

--- src/api/request.js
+++ src/api/request.js
@@ -1,3 +1,11 @@
+const apiKey = import.meta.env.VITE_APP_YOUTUBE_API_KEY;
+
 const requst = {
-  getSearchKeyWord: `/search`
+  getSearchKeyWord: `/search?part=snippet&maxResults=25&q=surfing&key=${apiKey}`,
+  getMostPopularVideos: `/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${apiKey}`,
+  getByChannelId: `/channels?part=snippet%2CcontentDetails%2Cstatistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw&key=${apiKey}`,
+  getVidoeId: `/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key=${apiKey}`,
+  getI18nRegions: `/i18nRegions?part=snippet&hl=es_MX&key=${apiKey}`
 };
+
+export default requst;
