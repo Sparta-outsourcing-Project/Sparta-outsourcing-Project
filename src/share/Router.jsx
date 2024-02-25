66072ee8425b0a9aba34ea56ba84daf0f3109bee
HyunjinKim <koujie11@gmail.com>
2024-02-25 13:34:40 +0900

    merge 받은 부분 정리

--- src/share/Router.jsx
+++ src/share/Router.jsx
@@ -1,6 +1,7 @@
 import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home';
 import List from '../pages/List';
+import Error from '../pages/Error';
 
 const router = createBrowserRouter([
   {
