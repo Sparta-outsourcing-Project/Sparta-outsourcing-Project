f8333982c0a5fe6ae26f4e5b6ba11867643e7ec4
HyunjinKim <koujie11@gmail.com>
2024-02-25 13:35:06 +0900

    fix : detail & list page 정리

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function Detail() {
-  return <div>DETAIL</div>;
+  return <>Detail</>;
 }
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import Header from '../components/layout/Header';
+import Footer from '../components/layout/Footer';
+import styled from 'styled-components';
+import CardList from '../components/list/CardList';
 
 export default function List() {
-  return <div>LIST</div>;
+  return (
+    <Wrap>
+      <Header />
+      <CardList />
+      <Footer />
+    </Wrap>
+  );
 }
+
+export const Wrap = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
