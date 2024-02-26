077634774344ed13637f8f1f6dfd4b841cde0dcd
Hyeryeon-Seo <0168968060@naver.com>
2024-02-26 20:17:53 +0900

    fix: api 사용될 주소- 변수 변경

--- src/api/request.js
+++ src/api/request.js
@@ -3,15 +3,15 @@ const videoSnippetFields = 'items(snippet(channelId,channelTitle,thumbnails(stan
 const channelStatisticsFields = 'items(statistics(subscriberCount,videoCount,viewCount))';
 
 const request = {
-  getSearchKeyWord: `/search?part=snippet&type=video&publishedAfter=2024-01-20T00:00:00Z&order=viewCount&maxResults=3&regionCode=KR&key=${apiKey}`, // maxResults 기능구현 후 바꾸기 (최대50으로?)
+  getSearchKeyWord: `/search?part=snippet&type=video&order=viewCount&regionCode=KR&key=${apiKey}`, // maxResults 기능구현 후 바꾸기 (최대50으로?)
   getMostPopularVideos: `/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${apiKey}`,
   getByChannelId: `/channels?part=snippet%2CcontentDetails%2Cstatistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw&key=${apiKey}`,
   getVidoeId: `/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key=${apiKey}`,
   getI18nRegions: `/i18nRegions?part=snippet&hl=es_MX&key=${apiKey}`,
   getChannelBannerURL: (channelId) => `/channels?part=brandingSettings&id=${channelId}&key=${apiKey}`,
 
-  getVideoSnippet: `/videos?part=snippet&key=${apiKey}`,
-  getChannelStatistics: `/channels?part=statistics&key=${apiKey}`,
+  getVideoSnippet: `/videos?part=snippet&key=${apiKey}&fields=${videoSnippetFields}`,
+  getChannelStatistics: `/channels?part=statistics&key=${apiKey}&fields=${channelStatisticsFields}`,
   getChannelSnippetStatistics: `/channels?part=snippet,statistics&key=${apiKey}`
 };
 
