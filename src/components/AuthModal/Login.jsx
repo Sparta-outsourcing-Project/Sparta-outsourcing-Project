4a3b604ce8201257c41ed1c823c7a7bd43244abd
innes-k <innes.kwak@gmail.com>
2024-02-27 02:24:48 +0900

    feat: 로그인, 회원가입 user정보 state설정

--- src/components/AuthModal/Login.jsx
+++ src/components/AuthModal/Login.jsx
@@ -1,7 +1,11 @@
 import * as St from './styles/Login.style';
 import logo from '../../assets/utrend_logo.png';
+import { useState } from 'react';
 
 const Login = ({ isLoginOpen, setIsLoginOpen, isSignUpOpen, setIsSignUpOpen }) => {
+  const [userId, setUserId] = useState('');
+  const [userPw, setUserPw] = useState('');
+
   // 닫기 버튼 클릭
   const onCloseButtonHandler = () => {
     isLoginOpen((prev) => !prev);
@@ -14,8 +18,18 @@ const Login = ({ isLoginOpen, setIsLoginOpen, isSignUpOpen, setIsSignUpOpen }) =
   };
 
   // 로그인 클릭 - 로그인 로직 추가 예정
-  const onLoginHandler = () => {
+  const onLoginHandler = (event) => {
+    event.preventDefault();
     setIsLoginOpen((prev) => !prev);
+  };
+
+  // 아이디, 비밀번호 입력값
+  const onUserId = (e) => {
+    setUserId(e.target.value);
+  };
+
+  const onUserPw = (e) => {
+    setUserPw(e.target.value);
   };
 
   return (
@@ -23,18 +37,18 @@ const Login = ({ isLoginOpen, setIsLoginOpen, isSignUpOpen, setIsSignUpOpen }) =
       {isLoginOpen && (
         <St.Background $isLoginOpen={isLoginOpen}>
           <St.Container>
-            <St.LoginWrapper>
+            <St.LoginWrapper onClick={onLoginHandler}>
               <button onClick={onCloseButtonHandler}>X</button>
               <img src={logo} alt="" />
               <St.InputBtnWrapper>
                 <p>로그인</p>
                 <St.InputContainer>
-                  <input placeholder="아이디" />
+                  <input placeholder="이메일" value={userId} onChange={onUserId} />
                 </St.InputContainer>
                 <St.InputContainer>
-                  <input placeholder="비밀번호" />
+                  <input placeholder="비밀번호" type="password" value={userPw} onChange={onUserPw} />
                 </St.InputContainer>
-                <button onClick={onLoginHandler}>로그인</button>
+                <button>로그인</button>
               </St.InputBtnWrapper>
 
               <St.CheckSignUp>
--- src/components/AuthModal/SignUp.jsx
+++ src/components/AuthModal/SignUp.jsx
@@ -1,15 +1,21 @@
 import * as St from './styles/Login.style';
 import { Background, InputBtnWrapper } from './styles/SignUp.style';
 import logo from '../../assets/utrend_logo.png';
+import { useState } from 'react';
 
 const SignUp = ({ isSignUpOpen, setIsSignUpOpen, isLoginOpen, setIsLoginOpen }) => {
+  const [userId, setUserId] = useState('');
+  const [userNickname, setUserNickname] = useState('');
+  const [userPw, setUserPw] = useState('');
+
   // 닫기 버튼 클릭
   const onCloseButtonHandler = () => {
     setIsSignUpOpen((prev) => !prev);
   };
 
   // 회원가입 클릭 - 가입 로직 추가 예정
-  const onSignUpHandler = () => {
+  const onSignUpHandler = (event) => {
+    event.preventDefault();
     setIsSignUpOpen((prev) => !prev);
   };
 
@@ -19,26 +25,39 @@ const SignUp = ({ isSignUpOpen, setIsSignUpOpen, isLoginOpen, setIsLoginOpen }) 
     setIsLoginOpen((prev) => !prev);
   };
 
+  // 아이디, 닉네임, 비밀번호 입력값
+  const onUserId = (e) => {
+    setUserId(e.target.value);
+  };
+
+  const onUserNickname = (e) => {
+    setUserNickname(e.target.value);
+  };
+
+  const onUserPw = (e) => {
+    setUserPw(e.target.value);
+  };
+
   return (
     <>
       {isSignUpOpen && (
         <Background $isSignUpOpen={isSignUpOpen}>
           <St.Container>
-            <St.LoginWrapper>
+            <St.LoginWrapper onClick={onSignUpHandler}>
               <button onClick={onCloseButtonHandler}>X</button>
               <img src={logo} alt="" />
               <InputBtnWrapper>
                 <p>회원가입</p>
                 <St.InputContainer>
-                  <input placeholder="아이디" />
+                  <input placeholder="이메일" value={userId} onChange={onUserId} />
                 </St.InputContainer>
                 <St.InputContainer>
-                  <input placeholder="닉네임" />
+                  <input placeholder="닉네임" value={userNickname} onChange={onUserNickname} />
                 </St.InputContainer>
                 <St.InputContainer>
-                  <input placeholder="비밀번호" />
+                  <input placeholder="비밀번호" type="password" value={userPw} onChange={onUserPw} />
                 </St.InputContainer>
-                <button onClick={onSignUpHandler}>회원가입</button>
+                <button>회원가입</button>
               </InputBtnWrapper>
 
               <St.CheckSignUp>
