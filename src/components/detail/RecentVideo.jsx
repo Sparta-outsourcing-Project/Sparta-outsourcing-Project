6c0b4bf28faa96ccc3be5d4603e7328e20aebfcf
HyunjinKim <koujie11@gmail.com>
2024-02-27 13:56:27 +0900

    feat : recentVideo 컴포넌트 추가

--- src/components/detail/RecentVideo.jsx
+++ src/components/detail/RecentVideo.jsx
@@ -1,20 +1,38 @@
-// import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { getMostPopularVideosInChannel } from '../../api/dataApi';
 
-// function RecentVideo({ video }) {
-//   return (
-//     <RecentVideoContainer>
-//       <ThumbnailImage src={video.thumbnailUrl} alt={video.title} />
-//     </RecentVideoContainer>
-//   );
-// }
+function RecentVideo({ channelId }) {
+  const [recentVideos, setRecentVideos] = useState([]);
+  // 최근 동영상 정보 불러오기
+  useEffect(() => {
+    const fetchChannelRecentVideos = async () => {
+      try {
+        const videos = await getMostPopularVideosInChannel(channelId);
+        console.log(videos);
+        setRecentVideos(videos);
+      } catch (error) {
+        console.error('Failed to fetch recent video info:', error.message);
+      }
+    };
+    fetchChannelRecentVideos();
+  }, [channelId]);
+  return (
+    <RecentVideoContainer>
+      {recentVideos.map((video) => {
+        <ThumbnailImage key={video.id} src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />;
+      })}
+    </RecentVideoContainer>
+  );
+}
 
-// export default RecentVideo;
+export default RecentVideo;
 
-// const RecentVideoContainer = styled.div`
-//   width: calc(100% / 3);
-// `;
+const RecentVideoContainer = styled.div`
+  width: calc(100% / 3);
+`;
 
-// const ThumbnailImage = styled.image`
-//   width: 100%;
-//   height: 180px;
-// `;
+const ThumbnailImage = styled.image`
+  width: 100%;
+  height: 180px;
+`;
